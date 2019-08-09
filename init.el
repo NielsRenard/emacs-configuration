6f12a930283dc05a87a1ea734fb7615b1eb0b1fa
Niels Renard <code.i.pushed@gmail.com>
2019-08-09 12:41:39 +0200

    Tweak autocomplete

--- init.el
+++ init.el
@@ -208,8 +208,9 @@
 (use-package company
   :ensure t
   :diminish company-mode
+  :bind ("TAB" . company-indent-or-complete-common)
   :config
-  (setq company-idle-delay 0.5)
+  (setq company-idle-delay 0.25)
   (setq company-show-numbers t)
   (setq company-tooltip-limit 10)
   (setq company-minimum-prefix-length 2)
@@ -217,7 +218,8 @@
   ;; invert the navigation direction if the the completion popup-isearch-match
   ;; is displayed on top (happens near the bottom of windows)
   (setq company-tooltip-flip-when-above t)
-  (global-company-mode))
+  (global-company-mode)
+  )
 
 (use-package company-lua
   :ensure t
